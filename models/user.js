cc203416ad4a77a5157cdc6cfa7d4560d7adafa0
Adrian Zniszczyński <a.zniszczynski@nowaera.pl>
2018-07-19 20:12:21 +0200

    Add email validation

--- models/user.js
+++ models/user.js
@@ -2,8 +2,18 @@
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         name: DataTypes.STRING,
-        mail: DataTypes.STRING,
+        mail: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
         password: DataTypes.STRING,
+        createdAt: {
+            type: DataTypes.DATE,
+        },
     });
     return User;
 };
