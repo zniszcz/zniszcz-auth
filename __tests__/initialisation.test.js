391aa5cf0bba88b6919dd8ea07e4a998ad72705f
Adrian Zniszczyński <a.zniszczynski@nowaera.pl>
2018-07-25 20:05:43 +0200

    Add testing welcome message

--- __tests__/initialisation.test.js
+++ __tests__/initialisation.test.js
@@ -1,19 +1,34 @@
 const request = require('supertest');
 const app = require('../app');
 
+const CURRENT_VERSION = require('child_process')
+    .execSync('git rev-parse --short HEAD')
+    .toString()
+    .trim();
+
 describe('HealthCheck', () => {
+
     test('should response GET method', () => {
         return request(app)
             .get('/')
             .expect(200);
     });
-});
 
-test('should have information about application version', (done) => {
-    return request(app).get('/')
-        .then((response) => {
-            expect(typeof response.body.version).toBe('string');
-            expect(response.body.length).not.toBe(0);
-            done();
-        });
+    test('should have information about application version', (done) => {
+        return request(app)
+            .get('/')
+            .then((response) => {
+                expect(response.body.version).toEqual(CURRENT_VERSION);
+                done();
+            });
+    });
+
+    test('should send welcome message', (done) => {
+        return request(app)
+            .get('/')
+            .then((response) => {
+                expect(response.body.message).toEqual('Welcome in Authorisation Service.');
+                done();
+            });
+    });
 });
